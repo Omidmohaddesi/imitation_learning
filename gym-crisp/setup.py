c921b6a044f580b50e5bf70c6d6b050d5869f601
Omid Mohaddesi <omidmohaddesi@gmail.com>
2020-03-10 16:58:57 -0400

    update api to get demand for each HC

--- gym-crisp/setup.py
+++ gym-crisp/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_crisp',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'matplotlib']  # And any other dependencies required
+      install_requires=['gym', 'numpy', 'matplotlib', 'flask', 'PyJWT', 'hashids']  # And any other dependencies required
       )
