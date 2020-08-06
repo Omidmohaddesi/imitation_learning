8e046ace56b8a24c694d3e2e5b3483daa0e38590
Omidmohaddesi <omidmohaddesi@gmail.com>
2020-08-06 12:33:10 -0400

    add player id to dataset

--- regression.py
+++ regression.py
@@ -44,17 +44,19 @@ if __name__ == '__main__':
     backlog = dataset.backlog
     shipment = dataset.shipment
 
-    data = pd.DataFrame(columns=['order', 'inventory', 'shipment', 'demand', 'backlog'])
+    data = pd.DataFrame(columns=['order', 'inventory', 'shipment', 'demand', 'backlog', 'player_id'])
 
     players_to_ignore = [10, 19, 11, 14]
 
     for i in range(0, 22):
         if i not in players_to_ignore:
             data = data.append(
-                pd.concat([order.iloc[i, 0:20], inventory.iloc[i, 0:20], shipment.iloc[i, 0:20],
-                          demand.iloc[i, 0:20], backlog.iloc[i, 0:20]],
-                          axis=1,
-                          keys=['order', 'inventory', 'shipment', 'demand', 'backlog']), ignore_index=True)
+                pd.concat([order.iloc[i, 0:20].reset_index(drop=True), inventory.iloc[i, 0:20].reset_index(drop=True),
+                           shipment.iloc[i, 0:20].reset_index(drop=True), demand.iloc[i, 0:20].reset_index(drop=True),
+                           backlog.iloc[i, 0:20].reset_index(drop=True),
+                           pd.Series(np.repeat(order.iloc[i, 21], 20))], axis=1,
+                          keys=['order', 'inventory', 'shipment', 'demand', 'backlog', 'player_id'])
+                , ignore_index=True)
 
     models = np.empty(shape=(0, 4), dtype=float)
 
